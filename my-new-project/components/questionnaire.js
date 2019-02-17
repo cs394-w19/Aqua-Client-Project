29d97d0e8ed1696818311aed683c4da6d2a35545
Efe Saatci <efesaatci2019@u.northwestern.edu>
2019-02-16 18:37:54 -0600

    Progress on implementing questionnare

--- my-new-project/components/questionnaire.js
+++ my-new-project/components/questionnaire.js
@@ -3,11 +3,24 @@ import { StyleSheet, Text, View } from 'react-native';
 import questions from '../questions.json';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {questions: questions.questions};
+        // let options = state.questions.map(q => {q.options.map(o => o ={o: false})})
 
+    }
     render() {
-        const questionItems = questions.questions.map( q => {
+        const questionItems = this.state.questions.map( q => {
+            console.log(q.options.keys())
+            const questionOptions = q.options.map(o => 
+                <Text>
+                    {Object.keys(o)}
+                </Text>
+            )
             return <View>
                     <Text> {q.text}</Text>
+                    {questionOptions}
+
             </View>
         });
         return (
