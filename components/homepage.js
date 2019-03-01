8e480f8479e52d17c8e4be8d986fcfb3a151bba5
Simeon <simeon@u.northwestern.edu>
2019-02-28 18:48:04 -0600

    firebase init

--- components/homepage.js
+++ components/homepage.js
@@ -1,27 +1,67 @@
-import React from 'react';
-import {StyleSheet, Text, View, ScrollView, TouchableWithoutFeedback} from 'react-native';
-import Questionnaire from './questionnaire';
+import React from "react"
+import {
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    TouchableWithoutFeedback
+} from "react-native"
+import Questionnaire from "./questionnaire"
+import * as firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB11yCSN9x1kE7Th3yZk_YnSBiizCyaVqQ",
+    authDomain: "https://glocal-1234.firebaseapp.com",
+    databaseURL: "https://glocal-1234.firebaseio.com",
+    storageBucket: "gs://glocal-1234.appspot.com"
+}
+
+firebase.initializeApp(firebaseConfig)
 
 export default class Homepage extends React.Component {
-    static navigationOptions = { header: null}
+    constructor() {
+        super()
+        this.state = {
+            user: null,
+            db: null,
+            loggedin: false
+        }
+    }
+
+    getUser(userId) {
+        if (userId == "simeon") {
+            firebase
+                .database()
+                .ref("users/" + userId)
+                .set({
+                    mood: "kill me"
+                })
+        }
+    }
+
+    componentDidMount() {
+        this.getUser("simeon")
+    }
+
+    static navigationOptions = { header: null }
 
     render() {
-        const {navigate} = this.props.navigation;
+        const { navigate } = this.props.navigation
         return (
             <View style={styles.container}>
                 <Text style={styles.header}>Glocal</Text>
-                <TouchableWithoutFeedback title="PlanTrip"
-                                          onPress={() => {
-                                              navigate("Questionnaire")
-                                          }}>
+                <TouchableWithoutFeedback
+                    title="PlanTrip"
+                    onPress={() => {
+                        navigate("Questionnaire")
+                    }}
+                >
                     <View style={styles.button}>
-                        <Text style={styles.buttonLabel}>
-                            Plan Your Trip!
-                        </Text>
+                        <Text style={styles.buttonLabel}>Plan Your Trip!</Text>
                     </View>
                 </TouchableWithoutFeedback>
             </View>
-        );
+        )
     }
 }
 
@@ -29,17 +69,17 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "white",
-        alignItems: 'center',
+        alignItems: "center"
     },
     button: {
         width: "85%",
         height: 75,
-        backgroundColor: '#1EA28A',
+        backgroundColor: "#1EA28A",
         margin: 10,
         borderRadius: 10,
         borderWidth: 0,
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center"
     },
     buttonLabel: {
         fontSize: 30,
@@ -51,6 +91,6 @@ const styles = StyleSheet.create({
         textAlign: "center",
         color: "#1EA28A",
         height: 300,
-        padding: 20,
+        padding: 20
     }
-});
+})
