9ccb372a01a907bff1be7c21597b52f5c5602747
Garion's Blade <yuxuanng@hotmail.com>
2019-03-07 20:52:25 -0600

    Dialog for naming itinerary

--- components/itineraryScreen.js
+++ components/itineraryScreen.js
@@ -5,9 +5,9 @@ import {
     Text,
     View,
     TouchableWithoutFeedback,
-    ScrollView
+    ScrollView, TextInput
 } from "react-native"
-
+import Dialog from "react-native-dialog";
 export default class ItineraryScreen extends React.Component {
     static navigationOptions = {
         title: 'Itineraries',
@@ -20,19 +20,40 @@ export default class ItineraryScreen extends React.Component {
         },
         tabBarLabel: 'Itineraries'
     }
-   render(){
-       const { navigate } = this.props.navigation;
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            dialogVisible: false
+        }
+    }
+
+    render() {
+        const {navigate} = this.props.navigation;
+        let itineraryName;
         return (
             <ScrollView>
                 <View style={styles.container}>
-                <TouchableWithoutFeedback onPress={()=>navigate('NewItinerary', {db:this.props.db, user:this.props.user})}>
-                    <View style={styles.newItineraryBtn}>
-                        <Text style={styles.newItineraryBtnText}>+</Text>
-                    </View>
-                </TouchableWithoutFeedback>
-                <Text style={styles.subHeader}>Upcoming Trips</Text>
-                <Text style={styles.subHeader}>Past Trips</Text>
+                    <TouchableWithoutFeedback
+                        onPress={() => this.setState({dialogVisible: true})}>
+                        <View style={styles.newItineraryBtn}>
+                            <Text style={styles.newItineraryBtnText}>+</Text>
+                        </View>
+                    </TouchableWithoutFeedback>
+                    <Text style={styles.subHeader}>Upcoming Trips</Text>
+                    <Text style={styles.subHeader}>Past Trips</Text>
                 </View>
+                <Dialog.Container visible={this.state.dialogVisible}>
+                    <Dialog.Title>Name Your Itinerary</Dialog.Title>
+                    <Dialog.Input placeholder="Itinerary Name"
+                                  value={itineraryName}
+                                  onChangeText={(text) =>itineraryName=text }/>
+                    <Dialog.Button label="Cancel" onPress={() => this.setState({dialogVisible: false})}/>
+                    <Dialog.Button label="Create" onPress={()=>{
+                        this.setState({dialogVisible: false})
+                        navigate('NewItinerary', {db: this.props.db, user: this.props.user, itineraryName: itineraryName})}
+                    }/>
+                </Dialog.Container>
             </ScrollView>
         )
     }
--- components/newItinerary.js
+++ components/newItinerary.js
@@ -8,7 +8,6 @@ import {
     TouchableWithoutFeedback,
     ScrollView
 } from "react-native"
-import SuggestedItem from "./suggestedItem"
 import Suggestions from "../suggestions.json"
 import profileQuestions from "../profileQuestions.json";
 
@@ -115,6 +114,26 @@ export default class newItinerary extends React.Component {
         return foundAttractions
     }
 
+    handleItemSelect = name => {
+        const state = this.state
+        const db = this.state.db
+        const user = this.state.user
+        let location;
+        if (state.suggestions.find(s => s.name === name)) {
+            location = state.suggestions.find(s => s.name === name)
+            location.selected = !location.selected
+            this.setState(state)
+        } else {
+            location = state.savedLocations.find(s => s.name === name)
+            location.selected = !location.selected
+            this.setState(state)
+        }
+    }
+
+    handleCreate = () =>{
+        this.setState({dialogVisible: true})
+    }
+
     render() {
         const {categories, suggestions, savedLocations} = this.state;
         const suggestedItems = suggestions.map(l => {
@@ -124,7 +143,7 @@ export default class newItinerary extends React.Component {
                     intersection={l.Categories.filter(x =>
                         categories.includes(x)
                     )}
-                    // handleItemSelect={this.handleItemSelect.bind(this)}
+                    handleItemSelect={this.handleItemSelect.bind(this)}
                     // handleGemClick={this.handleGemClick.bind(this)}
                 />
             )
@@ -136,8 +155,7 @@ export default class newItinerary extends React.Component {
                     intersection={l.Categories.filter(x =>
                         categories.includes(x)
                     )}
-                    // handleItemSelect={this.handleItemSelect.bind(this)}
-                    // handleGemClick={this.handleGemClick.bind(this)}
+                    handleItemSelect={this.handleItemSelect.bind(this)}
                 />
             )
         })
@@ -159,6 +177,11 @@ export default class newItinerary extends React.Component {
                         </View>
                     </ScrollView>
                 </View>
+                <TouchableWithoutFeedback onPress={()=>this.handleCreate()}>
+                    <View style={styles.createBtn}>
+                        <Text>Create Itinerary</Text>
+                    </View>
+                </TouchableWithoutFeedback>
             </View>
         )
     }
@@ -174,11 +197,18 @@ class CollectionItem extends React.Component {
                 </View>
                 <Image style={{width: 200, height: 170, borderBottomLeftRadius: 10, borderBottomRightRadius: 10}}
                        source={images[location.id]}/>
-                <TouchableWithoutFeedback>
+                {!location.selected &&
+                <TouchableWithoutFeedback onPress={() => this.props.handleItemSelect(location.name)}>
                     <View style={styles.colAddBtn}>
                         <Text style={styles.colAddBtnText}>Add to Itinerary</Text>
                     </View>
-                </TouchableWithoutFeedback>
+                </TouchableWithoutFeedback>}
+                {location.selected &&
+                <TouchableWithoutFeedback onPress={() => this.props.handleItemSelect(location.name)}>
+                    <View style={styles.colAddedBtn}>
+                        <Text style={styles.colAddBtnText}>Added</Text>
+                    </View>
+                </TouchableWithoutFeedback>}
             </View>
         )
     }
@@ -196,7 +226,7 @@ const styles = StyleSheet.create({
         marginLeft: 5
     },
     collectionsContainer: {
-        flex: 1,
+        flex: 5,
         backgroundColor: "#1EA28Acc",
     },
     collectionsScroll: {
@@ -204,7 +234,7 @@ const styles = StyleSheet.create({
         flex: 0
     },
     suggestionsContainer: {
-        flex: 1,
+        flex: 5,
         backgroundColor: "#555555cc"
     },
     colItem: {
@@ -237,7 +267,27 @@ const styles = StyleSheet.create({
         borderBottomLeftRadius: 10,
         borderBottomRightRadius: 10
     },
+    colAddedBtn: {
+        height: 40,
+        width: 200,
+        position: 'absolute',
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#1EA28Acc',
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10
+    },
     colAddBtnText: {
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    createBtn: {
+        backgroundColor: 'black',
+        flex: 1
+    },
+    createBtnText:{
+        color: 'white',
         fontSize: 20,
         textAlign: 'center'
     }
